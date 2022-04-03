69549adacab21dcd7eda19b48b180f071760710d
andrei-filip912 <filip.andrei912@gmail.com>
2022-04-03 17:38:49 +0200

    added k8s services for post and event-bus
    - updated event-bus\index.js address for posts, commented to others for now
    - added yaml config files
    - update event-bus address in posts\index.js

--- posts/index.js
+++ posts/index.js
@@ -22,7 +22,7 @@ app.post('/posts', async (req, res) => {
         title
     };
     
-    await axios.post('http://localhost:4005/events', {
+    await axios.post('http://event-bus-srv:4005/events', {
         type : 'PostCreated',
         data : {
             id, title
@@ -38,5 +38,6 @@ app.post('/events', (req, res) => {
 });
 
 app.listen((4000), () => {
+    console.log('Version: 0.0.5');
     console.log('Listening to port 4000');
 });
